c1a7d361b1dc24b4bbd452c58c24f22224170aa3
Jesse Cureton <jesse@jessecureton.me>
2022-03-23 20:22:13 -0400

    let jupyter start in the repo root

--- tools/jupyter/main.py
+++ tools/jupyter/main.py
@@ -1,8 +1,11 @@
+import os
 import re
 import sys
 
 from notebook.notebookapp import main
 
 if __name__ == "__main__":
+    if "BUILD_WORKSPACE_DIRECTORY" in os.environ:
+        os.chdir(os.environ["BUILD_WORKSPACE_DIRECTORY"])
     sys.argv[0] = re.sub(r"(-script\.pyw?|\.exe)?$", "", sys.argv[0])
     sys.exit(main())
