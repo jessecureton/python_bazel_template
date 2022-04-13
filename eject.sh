07b4c38739a73392667b3463c92ef009a2fafe9d
Jesse Cureton <jesse@jessecureton.me>
2022-04-12 21:52:26 -0400

    handle differences between bsd and gnu sed in eject script

--- eject.sh
+++ eject.sh
@@ -80,4 +80,18 @@ else
 fi
 
 msg "Updating all references to ${SOURCE_ARG} to ${DEST_ARG}"
-find ${REPO_ROOT} -type f -not -path "${REPO_ROOT}/.git/*" -not -path "${REPO_ROOT}/eject.sh" -exec sed -i "" -e "s/${SOURCE_ARG}/${DEST_ARG}/g" {} \;
+
+# BSD and GNU sed handle the `-i` argument for in-place edits differently
+INPLACEOPT=(-i)
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  # BSD sed expects an explicit null arg to follow the -i flag. We use an
+  # array expansion to accomplish this, since otherwise it gets swallowed
+  # if quoted in any quote character.
+  INPLACEOPT=(-i "")
+fi
+
+find ${REPO_ROOT} \
+    -type f \
+    -not -path "${REPO_ROOT}/.git/*" \
+    -not -path "${REPO_ROOT}/eject.sh" \
+    -exec sed "${INPLACEOPT[@]}" -e "s/${SOURCE_ARG}/${DEST_ARG}/g" {} \;
